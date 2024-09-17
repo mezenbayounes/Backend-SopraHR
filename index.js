014f09a7185c579426eaff1477f67eee8b39541b
BAYOUNES Mezen <mezen.bayounes@soprahr.com>
2024-09-17 16:32:59 +0100

    notif to user

--- index.js
+++ index.js
@@ -1,113 +1,254 @@
+
 import { config } from "dotenv";
+
+
+
 
 import { initDb } from "../BackEnd/dbConfig.js";
 
+
+
+
 import { connectionConfig } from "../BackEnd/dbConfig.js";
+
+
+
 
 import pkg from "pg";
 
+
+
+
 import cors from "cors";
+
+
+
 
 import express from "express";
 
+
+
+
 import authRouter from "./src/Router/authRouter.js";
+
+
+
 
 import userRouter from "./src/Router/userRouter.js";
 
+
+
+
 import EquipeRouter from "./src/Router/EquipeRouter.js";
+
+
+
 
 import PlateauRouter from "./src/Router/PlateauRouter.js";
 
+
+
+
 import congeRouter from "./src/Router/CongeRouter.js";
+
+
+
 
 import remoteRouter from "./src/Router/RemoteRouter.js";
 
+
+
+
 import notificationRouter from "./src/Router/NotificationRouter.js";
+
+
+
 
 import { WebSocketServer } from "ws"; // Import WebSocketServer
 
+
+
+
 import { Server } from "socket.io"; // Import socket.io
+
+
+
 
 import * as dotenv from "dotenv";
 
+
+
+
 const { Pool } = pkg;
+
+
+
 
 dotenv.config();
 
+
+
+
 config();
+
+
+
 
 const pool = new Pool(connectionConfig);
 
+
+
+
 initDb();
+
+
+
 
 //////////////////////////////////////////////////
 
+
+
+
 const app = express();
+
+
+
 
 app.use(express.json());
 
+
+
+
 app.use(express.urlencoded({ extended: true }));
+
+
+
 
 app.use(cors());
 
+
+
+
 app.use("/auth", authRouter);
+
+
+
 
 app.use("/user", userRouter);
 
+
+
+
 app.use("/equipe", EquipeRouter);
+
+
+
 
 app.use("/plateau", PlateauRouter);
 
+
+
+
 app.use("/conge", congeRouter);
+
+
+
 
 app.use("/remote", remoteRouter);
 
+
+
+
 app.use("/notification", notificationRouter);
+
+
+
 
 app.use("/assets", express.static("assets"));
 
+
+
+
 const PORT = 3000;
 
+
+
+
 const server = app.listen(PORT, () => {
+
   console.log(`Server running on port ${PORT}`);
+
 });
+
+
+
 
 // Initialize Socket.io on top of the HTTP server
 
+
+
+
 const io = new Server(server, {
+
   cors: {
+
     origin: "*", // Allow all origins (you can restrict this in production)
 
+
+
+
     methods: ["GET", "POST"],
+
   },
+
 });
 
 
 
-const connectedUsers = {};
 
-var idforsend="";
+
+
+
+
+const connectedUsers = {}; // Map to track connected users by userId
+
+var currectuser;
 
 // Socket.io connection handling
 
 io.on('connection', (socket) => {
 
+
+
+
   console.log('New client connected:', socket.id);
 
-  idforsend=socket.id;
+  console.log('New client connected:', socket.id);
 
-  // Register user connection by userId
-
-  socket.on('register', (userId) => {
-
-    connectedUsers[userId] = socket.id; // Map userId to socket.id
-
-    console.log(`User ${userId} connected with socket ID ${socket.id}`);
-
-  });
+  console.log('Current connectedUsers map:', connectedUsers);
 
 
 
+
+currectuser=socket.id;
+
+socket.on('sendUserID', (data) => {
+
+  console.log('Received message from client sendUserID:', data.userId);
+
+  console.log('New client connected id:', data.userId);
+
+
+
+
+  connectedUsers[ data.userId] = socket.id; 
+
+  console.log('Current connectedUsers map:', connectedUsers);
+
+
+
+
+});
 
   // Listen for messages from clients
 
@@ -115,12 +256,9 @@ io.on('connection', (socket) => {
 
     console.log('Received message from client:', data);
 
-
-
-
     // Optionally broadcast the message to other clients
 
-    socket.broadcast.emit('message', data);
+   // socket.broadcast.emit('message', data);
 
   });
 
@@ -143,6 +281,8 @@ io.on('connection', (socket) => {
 
         console.log(`User ${userId} disconnected`);
 
+        console.log('Current connectedUsers map:', connectedUsers);
+
         break;
 
       }
@@ -160,7 +300,12 @@ io.on('connection', (socket) => {
 
 export const sendNotificationToUser = (userId, notification) => {
 
-  const socketId = idforsend; // Get socket ID for the user
+  console.log('Current connectedUsers map:', connectedUsers);
+
+
+
+
+  const socketId = connectedUsers[userId]; // Get socket ID for the user
 
   if (socketId) {
 
@@ -175,9 +320,4 @@ export const sendNotificationToUser = (userId, notification) => {
   }
 
 };
-
-
-
-
-
 
--- src/api/conge/CongeController.js
+++ src/api/conge/CongeController.js
@@ -1,492 +1,1119 @@
+
 import pkg from "pg";
+
 import { connectionConfig } from "../../../dbConfig.js";
+
 import jwt from "jsonwebtoken";
+
 import * as dotenv from "dotenv";
+
 import { sendNotificationToUser } from "../../../index.js"; // Adjust the path as necessary
+
+
+
 
 import { createNotification } from "../Notification/NotificationController.js"; // Adjust the path as necessary
 
+
+
+
 const { Pool } = pkg;
+
 const pool = new Pool(connectionConfig);
 
+
+
+
 export const CreateConge = async (req, result) => {
+
   const { userId, cause, dateDebut, scDebut, dateFin, scFin, typeConge } =
+
     req.body;
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer.replace("Bearer ", "");
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
 
+
+
+
     // Token is valid and not expired, continue with the database operation
+
     const res = await pool.query(
+
       "INSERT INTO conge (user_id, cause, date_debut, sc_debut, date_fin, sc_fin, type_conge) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+
       [userId, cause, dateDebut, scDebut, dateFin, scFin, typeConge]
+
     );
+
+
+
 
     const newCongeId = res.rows[0].id; // Access the ID from the database response
 
+
+
+
     console.log(
+
       "Insertion successful. New congé ID:",
+
       newCongeId,
+
       "employee ID:",
+
       userId
+
     );
+
     result.send({ newCongeId });
+
     return 0;
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else {
+
       console.error("Error occurred during insertion:", error);
+
       throw error; // Rethrow the error to handle it further if needed
+
     }
+
   }
+
 };
 
+
+
+
 export const updateConge = async (req, result) => {
+
   const { congeId, cause, dateDebut, scDebut, dateFin, scFin, typeConge } =
+
     req.body;
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer.replace("Bearer ", "");
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
 
+
+
+
     // Token is valid and not expired, continue with the database operation
+
     const updateQuery = `
+
       UPDATE conge
+
       SET cause = $1, date_debut = $2, sc_debut = $3, date_fin = $4, sc_fin = $5, type_conge = $6, etat = 'EC'
+
       WHERE id = $7
+
       RETURNING id
+
     `;
 
+
+
+
     const res = await pool.query(updateQuery, [
+
       cause,
+
       dateDebut,
+
       scDebut,
+
       dateFin,
+
       scFin,
+
       typeConge,
+
       congeId,
+
     ]);
+
+
+
 
     const updatedCongeId = res.rows[0]; // Access the ID from the database response
 
+
+
+
     console.log("Update successful. Updated congé ID:", updatedCongeId);
+
     result.send({ updatedCongeId });
+
     return 0;
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else {
+
       console.error("Error occurred during update:", error);
+
       throw error; // Rethrow the error to handle it further if needed
+
     }
+
   }
+
 };
+
+
+
 
 export const getConge = async (req, result) => {
+
   const { congeId } = req.body;
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer.replace("Bearer ", "");
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
 
+
+
+
     // Token is valid and not expired, continue with the database operation
+
     console.log("Congé ID:", congeId);
 
+
+
+
     const query = "SELECT * FROM conge WHERE id = $1";
+
     const res = await pool.query(query, [congeId]);
 
+
+
+
     if (res.rows.length === 0) {
+
       console.log("Congé not found.");
+
       result.status(404).send({ message: "Congé not found." });
+
       return;
+
     }
+
+
+
 
     const congé = res.rows[0];
+
     console.log("Congé found:", congé);
+
     result.send(congé);
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else {
+
       console.error("Error occurred while fetching congé:", error);
+
       throw error; // Rethrow the error to handle it further if needed
+
     }
+
   }
+
 };
+
+
+
 
 export const getAllConges = async (req, result) => {
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer.replace("Bearer ", "");
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
+
+
+
 
     // Token is valid and not expired, continue with the database operation
+
     const query = "SELECT * FROM conge";
+
     const res = await pool.query(query);
 
+
+
+
     const congés = res.rows;
+
     console.log("Congés found:", congés);
+
     result.send(congés);
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else {
+
       console.error("Error occurred while fetching congés:", error);
+
       throw error; // Rethrow the error to handle it further if needed
+
     }
+
   }
+
 };
+
+
+
+
 
 
 export const validateConge = async (req, result) => {
 
+
+
+
   const { congeId, etat } = req.body;
 
+
+
+
   const tokenWithBearer = req.headers.authorization;
+
+
+
 
   const token = tokenWithBearer.replace("Bearer ", "");
 
 
 
 
+
+
+
+
+
+
   try {
 
+
+
+
     // Verify and decode the token
+
+
+
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 
 
 
+
+
+
+
+
+
     // Check if the token has expired
+
+
+
 
     if (Date.now() >= decoded.exp * 1000) {
 
+
+
+
       return result.status(401).json({ error: "Token expired" });
 
+
+
+
     }
+
+
+
+
+
+
 
 
 
 
     // Update the leave request (congé) status
 
+
+
+
     const updateQuery = `
+
+
+
 
       UPDATE conge
 
+
+
+
       SET etat = $1
+
+
+
 
       WHERE id = $2
 
-      RETURNING id
+
+
+
+      RETURNING id,date_debut,user_id
 
     `;
 
 
 
 
+
+
+
+
+
+
     const res = await pool.query(updateQuery, [etat, congeId]);
 
-    const updatedCongeId = congeId; // Access the ID from the database response
+
+
+
+    const resData = res.rows[0].date_debut;
+
+    const resUserId = res.rows[0].user_id;
+
+
+
+
+    const date = new Date(resData);
+
+    
+
+    // Format the date as dd/MM/yy
+
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+
+    const year = String(date.getFullYear()).slice(-2); // Last two digits of the year
+
+    
+
+    const formattedDate = `${day}/${month}/${year}`;
+
+
+
+
 
 
 
 
     // Notification logic after successful congé validation
 
-    const notificationContent = `Congé with ID ${congeId} has been ${etat}`;
+
+
+
+    const notificationContent = `Congé with date ${formattedDate} has been ${etat}`;
+
+
+
 
     const notificationType = "conge-validation"; // Custom type for congé validation notifications
+
+
+
 
     const userId = decoded.userId; // Assuming you have the user ID from the decoded JWT
 
 
 
 
+
+
+
+
+
+
     // Create notification record in the database
+
+
+
 
     const notificationData = {
 
+
+
+
       type: notificationType,
+
+
+
 
       content: notificationContent,
 
+
+
+
       user_id: userId
 
+
+
+
     };
+
+
+
 
     await createNotification(notificationData);
 
 
 
 
+
+
+
+
+
+
     // Broadcast notification to all connected WebSocket clients
+
+
+
 
     const notification = {
 
+
+
+
       type: notificationType,
+
+
+
 
       content: notificationContent,
 
+
+
+
       userId:userId
+
+
+
 
     };
 
-    sendNotificationToUser(userId,notification);
 
-    console.log("Congé validation successful. Updated congé ID:", updatedCongeId);
 
-    result.send({ updatedCongeId, notification });
+
+    sendNotificationToUser(resUserId,notification);
+
+
+
+
+    console.log("Congé validation successful. Updated congé ID:", congeId);
+
+
+
+
+    result.send({ resData, notification,resUserId });
+
+
+
 
   } catch (error) {
 
+
+
+
     if (error instanceof jwt.TokenExpiredError) {
+
+
+
 
       console.error("Token expired error:", error);
 
+
+
+
       return result.status(401).json({ error: "Token expired" });
 
+
+
+
     } else {
+
+
+
 
       console.error("Error occurred during congé validation:", error);
 
+
+
+
       return result.status(500).json({ error: "Internal Server Error" });
+
+
+
 
     }
 
+
+
+
   }
 
+
+
+
 };
+
+
+
+
 
 
 export const deleteConge = async (req, result) => {
+
   const { congeId } = req.body;
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer.replace("Bearer ", "");
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
 
+
+
+
     // Token is valid and not expired, continue with the database operation
+
     console.log("Congé ID:", congeId);
+
+
+
 
     const deleteQuery = "DELETE FROM conge WHERE id = $1";
 
+
+
+
     await pool.query(deleteQuery, [congeId]);
 
+
+
+
     console.log("Congé deleted successfully.");
+
     result.send({ message: "Congé deleted successfully." });
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else {
+
       console.error("Error occurred during congé deletion:", error);
+
       throw error; // Rethrow the error to handle it further if needed
+
     }
+
   }
+
 };
+
+
+
 
 export const getCongesByUserId = async (req, result) => {
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer ? tokenWithBearer.replace("Bearer ", "") : null;
 
+
+
+
   if (!token) {
+
     return result.status(401).json({ error: "Authorization token missing" });
+
   }
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
+
+
+
 
     // Extract user_id from request body
+
     const { user_id } = req.body;
 
+
+
+
     if (!user_id || isNaN(user_id)) {
+
       return result.status(400).json({ error: "Invalid or missing user ID" });
+
     }
+
+
+
 
     // Query to get conge records for the specific user_id
+
     const query = "SELECT * FROM conge WHERE user_id = $1  ORDER BY created_at DESC ";
+
     const res = await pool.query(query, [user_id]);
 
+
+
+
     const congés = res.rows;
+
     if (congés.length === 0) {
+
       return result.status(404).json({ message: "No congés found for this user" });
+
     }
+
+
+
 
     console.log("Congés found for user:", congés);
+
     result.json(congés);
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else if (error instanceof jwt.JsonWebTokenError) {
+
       console.error("JWT error:", error);
+
       return result.status(401).json({ error: "Invalid token" });
+
     } else {
+
       console.error("Error occurred while fetching congés by user ID:", error);
+
       result.status(500).json({ error: "Internal server error" });
+
     }
+
   }
+
 };
+
+
+
 
 export const getCongesByUserIds = async (req, result) => {
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer ? tokenWithBearer.replace("Bearer ", "") : null;
 
+
+
+
   if (!token) {
+
     return result.status(401).json({ error: "Authorization token missing" });
+
   }
 
+
+
+
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
+
+
+
 
     // Extract user_ids from request body
+
     const { user_ids } = req.body;
 
+
+
+
     if (!user_ids || !Array.isArray(user_ids) || user_ids.length === 0) {
+
       return result.status(400).json({ error: "Invalid or missing user IDs" });
+
     }
+
+
+
 
     // Build the query string with the correct number of placeholders
+
     const placeholders = user_ids.map((_, index) => `$${index + 1}`).join(", ");
+
     const query = `SELECT * FROM conge WHERE user_id IN (${placeholders}) ORDER BY created_at DESC`;
 
+
+
+
     // Execute the query with the user_ids as parameters
+
     const res = await pool.query(query, user_ids);
 
+
+
+
     const congés = res.rows;
+
     if (congés.length === 0) {
+
       return result.status(404).json({ message: "No congés found for these users" });
+
     }
+
+
+
 
     console.log("Congés found for users:", congés);
+
     result.json(congés);
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else if (error instanceof jwt.JsonWebTokenError) {
+
       console.error("JWT error:", error);
+
       return result.status(401).json({ error: "Invalid token" });
+
     } else {
+
       console.error("Error occurred while fetching congés by user IDs:", error);
+
       result.status(500).json({ error: "Internal server error" });
+
     }
+
   }
+
 };
+
+
+
 
 export const getCongesByManagerId = async (req, result) => {
+
   const tokenWithBearer = req.headers.authorization;
+
   const token = tokenWithBearer ? tokenWithBearer.replace("Bearer ", "") : null;
 
+
+
+
   if (!token) {
+
     return result.status(401).json({ error: "Authorization token missing" });
+
   }
+
+
+
 
   try {
+
     // Verify and decode the token
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+
+
+
     // Check if the token has expired
+
     if (Date.now() >= decoded.exp * 1000) {
+
       return result.status(401).json({ error: "Token expired" });
+
     }
+
+
+
 
     // Extract id_manager from request body
+
     const { id_manager } = req.body;
 
+
+
+
     if (!id_manager || isNaN(id_manager)) {
+
       return result.status(400).json({ error: "Invalid or missing manager ID" });
+
     }
+
+
+
 
     // Query to get employees list for the specific id_manager
+
     const employeesQuery = "SELECT employees FROM equipe WHERE id_manager = $1";
+
     const employeesRes = await pool.query(employeesQuery, [id_manager]);
 
+
+
+
     // Combine all employee arrays into a single array
+
     const employees = employeesRes.rows.flatMap(row => row.employees);
 
+
+
+
     if (employees.length === 0) {
+
       return result.status(404).json({ message: "No employees found for this manager" });
+
     }
+
+
+
 
     // Query to get conge records for the list of employee IDs
+
     const placeholders = employees.map((_, index) => `$${index + 1}`).join(", ");
+
     const congesQuery = `SELECT * FROM conge WHERE user_id IN (${placeholders})  ORDER BY created_at DESC`;
+
     const congesRes = await pool.query(congesQuery, employees);
 
+
+
+
     const conges = congesRes.rows;
+
     if (conges.length === 0) {
+
       return result.status(404).json({ message: "No congés found for these employees" });
+
     }
 
+
+
+
     console.log("Congés found for employees:", conges);
+
     result.json(conges);
+
   } catch (error) {
+
     if (error instanceof jwt.TokenExpiredError) {
+
       console.error("Token expired error:", error);
+
       return result.status(401).json({ error: "Token expired" });
+
     } else if (error instanceof jwt.JsonWebTokenError) {
+
       console.error("JWT error:", error);
+
       return result.status(401).json({ error: "Invalid token" });
+
     } else {
+
       console.error("Error occurred while fetching congés by manager ID:", error);
+
       result.status(500).json({ error: "Internal server error" });
+
     }
+
   }
+
 };
+
+
+
+
+
 
